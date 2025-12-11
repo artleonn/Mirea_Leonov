42a39bb3cd3668b87f93a9e26f3cca343b55f148
Артём <artem.272510@gmail.com>
2025-12-11 17:26:26 +0300

    Практика 12

---
+++ src/ThrowsDemo.java
@@ -0,0 +1,7 @@
+public class ThrowsDemo {
+    public void getDetails(String key) {
+        if (key == null) {
+            throw new NullPointerException("null key in getDetails");
+        }
+    }
+}
---
+++ src/ThrowsDemoModified.java
@@ -0,0 +1,13 @@
+ppublic class ThrowsDemoModified {
+    public void printMessage(String key) {
+        String message = getDetails(key);
+        System.out.println(message);
+    }
+
+    public String getDetails(String key) {
+        if (key == null) {
+            throw new NullPointerException("null key in getDetails");
+        }
+        return "data for " + key;
+    }
+}
